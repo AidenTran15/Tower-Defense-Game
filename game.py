960317bd2893d7b0a5136ba9f155504c2a807f5e
Aiden Tran <aidenkiettran@gmail.com>
2022-01-21 14:35:18 +1100

    scale the background image to fit the screen

--- game.py
+++ game.py
@@ -3,14 +3,15 @@ import os
 
 class Game:
     def __init__(self):
-        self.width = 1100
-        self.height = 800
+        self.width = 1200
+        self.height = 700
         self.win = pygame.display.set_mode((self.width, self.height))
         self.enemys = []
         self.towers = []
         self.lives = 10
         self.money = 100
         self.bg = pygame.image.load(os.path.join("game_assets", "background.png"))
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
         self.clicks = [] # remove
 
     def run(self):
